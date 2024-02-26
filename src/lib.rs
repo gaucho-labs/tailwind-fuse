37a51e4276bea730bf03cc63a8f06ba38cd6b5a0
Nico Burniske <nicoburniske@gmail.com>
2024-02-26 09:28:12 -0500

    first macro attempt

---
+++ src/lib.rs
@@ -0,0 +1,63 @@
+#[macro_export]
+macro_rules! tw {
+    ($($item:expr),+ $(,)?) => {{
+        let mut result = String::new();
+        $(
+            let tailwind_class = $item.to_tailwind_class();
+            if let Some(class) = tailwind_class {
+                let class = class.trim();
+                if !class.is_empty() {
+                    if !result.is_empty() { result.push(' '); }
+                    result.push_str(class);
+                }
+            }
+        )*
+        result
+    }};
+}
+
+trait ToTailwindClass<'a> {
+    fn to_tailwind_class(&'a self) -> Option<&'a str>;
+}
+
+impl<'a> ToTailwindClass<'a> for String {
+    fn to_tailwind_class(&'a self) -> Option<&'a str> {
+        Some(self.as_str())
+    }
+}
+
+impl<'a> ToTailwindClass<'a> for str {
+    fn to_tailwind_class(&'a self) -> Option<&'a str> {
+        Some(self)
+    }
+}
+
+impl<'a> ToTailwindClass<'a> for Option<String> {
+    fn to_tailwind_class(&'a self) -> Option<&'a str> {
+        self.as_deref()
+    }
+}
+
+impl<'a> ToTailwindClass<'a> for Option<&'a str> {
+    fn to_tailwind_class(&'a self) -> Option<&'a str> {
+        *self
+    }
+}
+
+#[test]
+fn test_tw() {
+    assert_eq!(tw!("hello"), "hello");
+    assert_eq!(tw!("one", "two"), "one two")
+}
+
+#[test]
+fn test_option() {
+    let classes = tw!(
+        "text-sm",
+        Some("font-bold"),
+        None::<String>,
+        "bg-white",
+        Some(" ")
+    );
+    assert_eq!(classes, "text-sm font-bold bg-white");
+}
