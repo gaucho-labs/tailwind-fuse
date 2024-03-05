9b9821710a63acfc48fcc45b41dfbcc8006dc935
Nico Burniske <nicoburniske@gmail.com>
2024-03-05 18:22:03 -0500

    axum example

---
+++ example/start-axum/src/app.rs
@@ -0,0 +1,54 @@
+use crate::{
+    button::{BtnSize, BtnVariant, Button},
+    error_template::{AppError, ErrorTemplate},
+};
+use leptos::*;
+use leptos_meta::*;
+use leptos_router::*;
+
+#[component]
+pub fn App() -> impl IntoView {
+    // Provides context that manages stylesheets, titles, meta tags, etc.
+    provide_meta_context();
+
+    view! {
+        <Stylesheet id="leptos" href="/pkg/start-axum.css"/>
+
+        // sets the document title
+        <Title text="Welcome to Leptos"/>
+
+        // content for this welcome page
+        <Router fallback=|| {
+            let mut outside_errors = Errors::default();
+            outside_errors.insert_with_default_key(AppError::NotFound);
+            view! { <ErrorTemplate outside_errors/> }.into_view()
+        }>
+            <main>
+                <Routes>
+                    <Route path="" view=HomePage/>
+                </Routes>
+            </main>
+        </Router>
+    }
+}
+
+/// Renders the home page of your application.
+#[component]
+fn HomePage() -> impl IntoView {
+    // Creates a reactive value to update the button
+    let (count, set_count) = create_signal(0);
+    let on_click = move |_| set_count.update(|count| *count += 1);
+
+    view! {
+        <div class="flex items-center gap-4 p-10">
+            <Button on:click=on_click size=BtnSize::Lg>
+                "Click Me: "
+                {count}
+            </Button>
+            <Button on:click=on_click size=BtnSize::Sm variant=BtnVariant::Secondary>
+                "Click Me: "
+                {count}
+            </Button>
+        </div>
+    }
+}
---
+++ example/start-axum/src/button.rs
@@ -0,0 +1,58 @@
+use leptos::*;
+use tailwind_util::*;
+
+#[component]
+pub fn Button(
+    #[prop(into, optional)] size: Option<BtnSize>,
+    #[prop(into, optional)] variant: Option<BtnVariant>,
+    #[prop(into, optional)] class: Option<String>,
+    /// Arbitrary additional attributes.
+    #[prop(attrs)]
+    attributes: Vec<(&'static str, Attribute)>,
+    children: Children,
+) -> impl IntoView {
+    let btn = Btn {
+        size: size.unwrap_or_default(),
+        variant: variant.unwrap_or_default(),
+    };
+
+    let class = class.map_or(btn.to_class(), |c| btn.with_class(c));
+
+    view! {
+        <button {..attributes} class=class>
+            {children()}
+        </button>
+    }
+}
+
+#[derive(TwClass, Default)]
+#[tw(
+    class = "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors disabled:pointer-events-none disabled:opacity-50"
+)]
+pub struct Btn {
+    variant: BtnVariant,
+    size: BtnSize,
+}
+
+#[derive(TwVariant)]
+pub enum BtnVariant {
+    #[tw(
+        default,
+        class = "bg-indigo-600 text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+    )]
+    Default,
+    #[tw(
+        class = "bg-white text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+    )]
+    Secondary,
+}
+
+#[derive(TwVariant)]
+pub enum BtnSize {
+    #[tw(class = "h-8 px-3 text-xs")]
+    Sm,
+    #[tw(default, class = "h-9 px-4 py-2")]
+    Default,
+    #[tw(class = "h-10 px-8")]
+    Lg,
+}
