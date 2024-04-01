385b563d02fc2025e498cbf3570bf88c83f095c6
Nico Burniske <nicoburniske@gmail.com>
2024-04-01 12:37:07 -0400

    simplify MaybeIntoTailwindClass

--- fuse/src/core/join.rs
+++ fuse/src/core/join.rs
@@ -8,27 +8,21 @@
 #[macro_export]
 macro_rules! tw_join {
      ($item:expr) => {{
-        use $crate::MaybeIntoTailwindClass;
-        let tailwind_class = $item.to_tailwind_class();
-        if let Some(class) = tailwind_class {
-            class.trim().into()
-        } else {
-            String::new()
-        }
+        use $crate::AsTailwindClass;
+        let tailwind_class = $item.as_tailwind_class();
+        tailwind_class.trim().to_string()
     }};
     ($($item:expr),+ $(,)?) => {{
-        use $crate::MaybeIntoTailwindClass;
+        use $crate::AsTailwindClass;
         let mut result = String::new();
         $(
             // Long lived expressions.
-            let item = $item;
-            let tailwind_class = item.to_tailwind_class();
-            if let Some(class) = tailwind_class {
-                let class = class.trim();
-                if !class.is_empty() {
-                    if !result.is_empty() { result.push(' '); }
-                    result.push_str(class);
-                }
+            let class = $item;
+            let class = class.as_tailwind_class();
+            let class = class.trim();
+            if !class.is_empty() {
+                if !result.is_empty() { result.push(' '); }
+                result.push_str(class);
             }
         )*
         result
@@ -37,21 +31,10 @@ macro_rules! tw_join {
 
 #[test]
 fn test_tw() {
-    assert_eq!(tw_join!("hello"), "hello");
-    assert_eq!(tw_join!("one", "two"), "one two")
-}
-
-#[test]
-fn test_option() {
-    let classes = tw_join!(
-        "text-sm",
-        Some("font-bold"),
-        Some("ring").filter(|_| false),
-        None::<String>,
-        "bg-white",
-        Some(" "),
-        "".to_string(),
-        "italic text-green-500"
+    assert_eq!(tw_join!("one"), "one");
+    assert_eq!(tw_join!("one", "two"), "one two");
+    assert_eq!(
+        tw_join!(" one", "two ", " three".to_string()),
+        "one two three"
     );
-    assert_eq!(classes, "text-sm font-bold bg-white italic text-green-500");
 }
--- fuse/src/core/mod.rs
+++ fuse/src/core/mod.rs
@@ -6,31 +6,16 @@ pub mod merge;
 /// Used to extract a &str from a type
 ///
 /// Implement this trait for your type to use it with the [`tw_join!`] and [`tw_merge!`] macros
-pub trait MaybeIntoTailwindClass<'a> {
-    /// Attempt to extract a Tailwind class
-    fn to_tailwind_class(&'a self) -> Option<&'a str>;
+pub trait AsTailwindClass<'a> {
+    /// Extract a Tailwind class
+    fn as_tailwind_class(&'a self) -> &'a str;
 }
 
-impl<'a> MaybeIntoTailwindClass<'a> for String {
-    fn to_tailwind_class(&'a self) -> Option<&'a str> {
-        Some(self.as_str())
-    }
-}
-
-impl<'a> MaybeIntoTailwindClass<'a> for str {
-    fn to_tailwind_class(&'a self) -> Option<&'a str> {
-        Some(self)
-    }
-}
-
-impl<'a> MaybeIntoTailwindClass<'a> for Option<String> {
-    fn to_tailwind_class(&'a self) -> Option<&'a str> {
-        self.as_deref()
-    }
-}
-
-impl<'a> MaybeIntoTailwindClass<'a> for Option<&'a str> {
-    fn to_tailwind_class(&'a self) -> Option<&'a str> {
-        *self
+impl<T> AsTailwindClass<'_> for T
+where
+    T: AsRef<str>,
+{
+    fn as_tailwind_class(&self) -> &str {
+        self.as_ref()
     }
 }
