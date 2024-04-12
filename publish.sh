2e08dd13abfd9b56c4c9f2f7ce36cbfde6b9f01c
Nico Burniske <nicoburniske@gmail.com>
2024-04-12 17:02:55 -0400

    update version for local variant macro dependency and fix publish script to abort

--- publish.sh
+++ publish.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+cargo check
+if [ $? -ne 0 ]; then
+    echo "Error: cargo check failed. Publishing aborted."
+    exit 1
+fi
+
+echo "Publishing variant-macro"
 cargo publish --manifest-path ./variant-macro/Cargo.toml $@
 
+echo "Publishing crate"
 cargo publish --manifest-path ./fuse/Cargo.toml $@
