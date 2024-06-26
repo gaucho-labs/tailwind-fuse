c0dec756f454235083434bf8e5ae4b4615857110
Nico Burniske <nicoburniske@gmail.com>
2024-06-26 14:12:37 -0400

    simplify join! macro

--- fuse/src/core/join.rs
+++ fuse/src/core/join.rs
@@ -7,97 +7,23 @@
 /// If you want a custom type to be used with this macro, implement the [`crate::AsTailwindClass`] trait.
 #[macro_export]
 macro_rules! tw_join {
-     ($item:expr) => {{
+    ($item:expr) => {{
         use $crate::AsTailwindClass;
-        let tailwind_class = $item.as_class();
-        tailwind_class.trim().to_string()
-    }};
-    ($a:expr, $b:expr) => {{
-        use $crate::AsTailwindClass;
-        let a = $a;
-        let a_class = a.as_class().trim();
-        let b = $b;
-        let b_class = b.as_class().trim();
-        format!(
-            "{}{}{}",
-            a_class,
-            if b_class.is_empty() { "" } else { " " },
-            b_class
-        )
-    }};
-    ($a:expr, $b:expr, $c:expr) => {{
-        use $crate::AsTailwindClass;
-        let a = $a;
-        let a_class = a.as_class().trim();
-        let b = $b;
-        let b_class = b.as_class().trim();
-        let c = $c;
-        let c_class = c.as_class().trim();
-        format!(
-            "{}{}{}{}{}",
-            a_class,
-            if b_class.is_empty() { "" } else { " " },
-            b_class,
-            if c_class.is_empty() { "" } else { " " },
-            c_class
-        )
-    }};
-    ($a:expr, $b:expr, $c:expr, $d:expr) => {{
-        use $crate::AsTailwindClass;
-        let a = $a;
-        let a_class = a.as_class().trim();
-        let b = $b;
-        let b_class = b.as_class().trim();
-        let c = $c;
-        let c_class = c.as_class().trim();
-        let d = $d;
-        let d_class = d.as_class().trim();
-        format!(
-            "{}{}{}{}{}{}{}",
-            a_class,
-            if b_class.is_empty() { "" } else { " " },
-            b_class,
-            if c_class.is_empty() { "" } else { " " },
-            c_class,
-            if d_class.is_empty() { "" } else { " " },
-            d_class
-        )
-    }};
-    ($a:expr, $b:expr, $c:expr, $d:expr, $e:expr) => {{
-        use $crate::AsTailwindClass;
-        let a = $a;
-        let a_class = a.as_class().trim();
-        let b = $b;
-        let b_class = b.as_class().trim();
-        let c = $c;
-        let c_class = c.as_class().trim();
-        let d = $d;
-        let d_class = d.as_class().trim();
-        let e = $e;
-        let e_class = e.as_class().trim();
-        format!(
-            "{}{}{}{}{}{}{}{}{}",
-            a_class,
-            if b_class.is_empty() { "" } else { " " },
-            b_class,
-            if c_class.is_empty() { "" } else { " " },
-            c_class,
-            if d_class.is_empty() { "" } else { " " },
-            d_class,
-            if e_class.is_empty() { "" } else { " " },
-            e_class
-        )
+        $item.as_class().trim().to_string()
     }};
     ($($item:expr),+ $(,)?) => {{
         use $crate::AsTailwindClass;
-        let mut result = String::new();
+        let mut result = String::with_capacity(
+            0 $(+ $item.as_class().len())*
+        );
         $(
-            // Long lived expressions.
             let class = $item;
             let class = class.as_class();
             let class = class.trim();
             if !class.is_empty() {
-                if !result.is_empty() { result.push(' '); }
+                if !result.is_empty() {
+                    result.push(' ');
+                }
                 result.push_str(class);
             }
         )*
